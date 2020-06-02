bd0f1921e5eaa8b04c463d52ba92f0b32ce1c93c
Degusto <thepilarap@gmail.com>
2020-06-02 21:37:26 +0200

    Extracted types

---
+++ src/models.ts
@@ -0,0 +1,12 @@
+export const enum ActionType {
+  Select = "Select",
+  Deselect = "Deselect",
+}
+
+export type ServiceYear = 2020 | 2021 | 2022;
+export type ServiceType =
+  | "Photography"
+  | "VideoRecording"
+  | "BlurayPackage"
+  | "TwoDayEvent"
+  | "WeddingSession";
